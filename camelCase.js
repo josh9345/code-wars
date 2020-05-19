f8154d187d95aa2e88c9a3130bab5159ff2d54bf
Joshua Reed <joshualreed93@gmail.com>
2020-05-19 14:30:49 -0400

    camelCase complete

---
+++ camelCase.js
@@ -0,0 +1,24 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// solution("camelCasing")  ==  "camel Casing"
+
+// 6ky
+
+// URL: https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
+
+function solution(string) {
+  let array = [...string];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i].toUpperCase()) {
+      array.splice(i, 0, " ");
+      i++;
+    }
+  }
+  console.log(array.join(""));
+  // return(array.join(''))
+}
+solution("camelCase");
+solution("camelCaseYay");
+solution("thisIsFunky");
+solution("hiHowAreYou");
